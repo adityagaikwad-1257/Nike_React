610c19e4fc828e8ae17b22982cd435df219239ea
AdityaGaikwad <adityagaikwad@wdimails.com>
2024-12-09 13:05:36 +0530

    Another initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,29 @@
+const Button = ({label, iconUrl, bgColor, borderColor, textColor, fullWidth}) =>{
+  return (
+    <button className=
+              {
+                `flex justify-center
+                items-center gap-2 px-7 py-4 border font-montserrat
+                text-lg leading-none
+                active:bg-coral-red-500
+                ${bgColor
+                  ? `${bgColor} ${borderColor} ${textColor}`
+                  : `bg-coral-red
+                     text-white border-coral-red`} rounded-full ${fullWidth && "w-full"}`
+              }>
+        {label}
+
+        {iconUrl && <img
+            className="ml-2 w-5 h-5 rounded-full"
+            src={iconUrl}
+            alt="Arrow right"
+            width={5}
+            height={5}
+        />}
+
+        
+    </button>
+  )
+}
+
+export default Button
---
+++ src/components/Nav.jsx
@@ -0,0 +1,45 @@
+import {headerLogo} from '../assets/images'
+import {hamburger} from '../assets/icons'
+import { navLinks } from '../constants'
+
+const Nav = () => {
+  return (
+    <header className='padding-x py-10 absolute z-10 w-full'>
+        <nav className='max-container flex justify-between items-center'>
+                <a href="/">
+                    <img
+                        src={headerLogo}
+                        alt='Logo'
+                        width={130}
+                        height={29}
+                    />
+                </a>
+                <div>
+                    <ul className='flex-1 flex justify-center items-center gap-12 max-lg:hidden'>
+                        {navLinks.map((item) => (
+                            <li key={item.label}>
+                                <a
+                                    href={item.href}
+                                    className='font-montserrat
+                                    leading-normal text-lg
+                                    text-black hover:text-white hover:bg-coral-red py-3 px-4 rounded-full'>
+                                    {item.label}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className='hidden max-lg:block'>
+                    <img
+                        src={hamburger}
+                        alt='Hamburger'
+                        width={25}
+                        height={25}
+                    />
+                </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
